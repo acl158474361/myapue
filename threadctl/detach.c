e34469654b1d24f3eafec18a15aad421140591c2
icecream <you@example.com>
2019-11-06 19:27:16 +0800

    timeout

--- threadctl/detach.c
+++ threadctl/detach.c
@@ -1,14 +1,15 @@
 #include "apue.h"
 #include <pthread.h>
 
-int makethread(void* (*fn)(void*),void *arg,pthread_t *tid){
+int makethread(void* (*fn)(void*),void *arg){
+    pthread_t tid;
     pthread_attr_t pattr;
     int err;
     if ( (err = pthread_attr_init(&pattr))!=0)
         return err;
     err = pthread_attr_setdetachstate(&pattr,PTHREAD_CREATE_DETACHED);
     if(err == 0)
-        err = pthread_create(tid,&pattr,fn,arg);
+        err = pthread_create(&tid,&pattr,fn,arg);
     
     pthread_attr_destroy(&pattr);
     return err;
