cd818f434debb72cd56507d4d1fae5a400c5c369
icecream <158474361@qq.com>
2020-01-15 06:31:02 -0800

    memstr

--- stdio/memstr.c
+++ stdio/memstr.c
@@ -13,7 +13,7 @@ int main(){
 	if ((fp = fmemopen(buf, BSZ, "w+")) == NULL)
 		err_sys("fmemopen failed");
 	printf("initial buffer contents: %s\n", buf);
-	fprintf(fp, "hello, world");
+	fprintf(fp, "hello, world"); //此时流中数据的大小为12
 	printf("before flush: %s\n", buf);
 	fflush(fp);
 	printf("after fflush: %s\n", buf);
@@ -22,7 +22,7 @@ int main(){
 	memset(buf, 'b', BSZ-2);
 	buf[BSZ-2] = '\0';
 	buf[BSZ-1] = 'X';
-	fprintf(fp, "hello, world");
+	fprintf(fp, "hello, world"); //此时流中数据的大小为24
 	fseek(fp, 0, SEEK_SET);
 	printf("after  fseek: %s\n", buf);
 	printf("len of string in buf = %ld\n", (long)strlen(buf));
@@ -30,7 +30,7 @@ int main(){
 	memset(buf, 'c', BSZ-2);
 	buf[BSZ-2] = '\0';
 	buf[BSZ-1] = 'X';
-	fprintf(fp, "hello, world");
+	fprintf(fp, "hello, worldhello, world"); //没有超过流中数据的大小24 不加null 后面的内容都会被打印出来
 	fclose(fp);
 	printf("after fclose: %s\n", buf);
 	printf("len of string in buf = %ld\n", (long)strlen(buf));
